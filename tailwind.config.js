80a9829a465824476d2fe7a3add54dbfa44853b8
sikander-afzaal <sikander.aaa@gmail.com>
2023-04-18 17:52:11 +0500

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ module.exports = {
       colors: {
         primary: "#f8a11e",
         dark: "#292929",
+        secondary: "#e1b387",
       },
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
       backgroundImage: {
         hero: 'url("./assets/hero.png")',
+        leader: 'url("/assets/leader.png")',
       },
     },
   },
